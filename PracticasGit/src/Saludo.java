2cc495455908db11f7fb6d1f5854d534a5b12d66
oscarc-ugarte <cesar.ugrt@gmail.com>
2020-08-24 20:17:19 -0300

    Comentario del push.

---
+++ PracticasGit/src/Saludo.java
@@ -0,0 +1,12 @@
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "" ;
+		System.out.println("Hola Mundo.");
+		System.out.println("Conectado a git"); //Agrego un comentario
+		
+	}
+
+}
